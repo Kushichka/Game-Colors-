ec04320718c226634429e55f718275abe7ecc8b6
Kushichka <kushichka@gmail.com>
2022-01-09 18:44:10 +0100

    gulpfile optimized, autoreload page, version 1.0.3

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-const gulp = require('gulp') // gulp
-const sass = require('gulp-sass')(require('sass')) // sass compiller 
-const cleanCSS = require('gulp-clean-css') // minify css
-const rename = require('gulp-rename') // rename files (.min)
-const uglify = require('gulp-uglify') // minify js
-const babel = require('gulp-babel') // new version js to old
-const del = require('del') // delete dist dirrectory
-const sourcemaps = require('gulp-sourcemaps') // sourcemap for easly debugging
-const autoprefixer = require('gulp-autoprefixer');
+const {src, dest, series, parallel, watch} = require('gulp')    // gulp
+      sass = require('gulp-sass')(require('sass'))              // sass compiller 
+      cleanCSS = require('gulp-clean-css')                      // minify css
+      rename = require('gulp-rename')                           // rename files (.min)
+      uglify = require('gulp-uglify')                           // minify js
+      babel = require('gulp-babel')                             // new version js to old
+      del = require('del')                                      // delete dist dirrectory
+      sourcemaps = require('gulp-sourcemaps')                   // sourcemap for easly debugging
+      autoprefixer = require('gulp-autoprefixer')               // auto css prefixes for others brousers
+      browserSync = require('browser-sync').create()            // autoreload page
 
-const path = { // paths to files
+// paths to files //
+const path = {
     style: {
         src: 'src/style/**/*.scss',
         dest: 'dist/style/'
@@ -16,15 +18,32 @@ const path = { // paths to files
     script: {
         src: 'src/script/**/*.js',
         dest: 'dist/script/'
+    },
+    index: {
+        src: 'src/index.html',
+        dest: 'dist/'
     }
 }
 
-function clean() { // dist cleaner
+
+// browser-sync server //
+function webServer() {
+    browserSync.init({
+        server: {
+            baseDir: './dist',
+            notify: false
+        }
+    });
+}
+
+// dist cleaner //
+function clean() {
     return del(['dist'])
 }
 
-function style() { // style's compiller
-    return gulp.src(path.style.src)
+// style's compiller //
+function style() {
+    return src(path.style.src)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
@@ -38,11 +57,13 @@ function style() { // style's compiller
             suffix: '.min'
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.style.dest))
+        .pipe(dest(path.style.dest))
+        .pipe(browserSync.stream())
 }
 
-function script() { // script's compiller
-    return gulp.src(path.script.src)
+// script's compiller //
+function script() {
+    return src(path.script.src)
       .pipe(sourcemaps.init())
       .pipe(babel({
         presets: ['@babel/env']
@@ -53,19 +74,35 @@ function script() { // script's compiller
         suffix: '.min'
       }))
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(path.script.dest))
+      .pipe(dest(path.script.dest))
+      .pipe(browserSync.stream())
 }
 
-function watch() { // gulp watcher
-    gulp.watch(path.style.src, style);
-    gulp.watch(path.script.src, script);
+// index compiller //
+function index() {
+    return src(path.index.src)
+      .pipe(sourcemaps.init())
+      .pipe(dest(path.index.dest))
+      .pipe(browserSync.stream())
 }
 
-const build = gulp.series(clean, gulp.parallel(style, script), watch) // build a project
+// gulp watcher //
+function watcher() {
+    watch(path.style.src, style).on('change', browserSync.reload);
+    watch(path.script.src, script).on('change', browserSync.reload);
+    watch(path.index.src, index).on('change', browserSync.reload);
+}
 
+// build a project //
+const build = series(clean, style, script, index, parallel(webServer, watcher))
+
+
+// exports //
 exports.style = style
 exports.script = script
+exports.index = index
 exports.clean = clean
-exports.watch = watch
+exports.watcher = watcher
+exports.webServer = webServer
 exports.build = build
 exports.default = build
